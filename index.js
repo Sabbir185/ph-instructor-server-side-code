d4843665d6c658c4f7091720d4b9fbcf315b54bd
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-06-21 12:31:10 +0600

    update done

--- index.js
+++ index.js
@@ -20,31 +20,62 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
     const planCollection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_PLAN}`);
+
+    const paymentCollection = client.db(`${process.env.DB_DATABASE}`).collection(`${process.env.DB_PAY}`);
+
     // perform actions on the collection object
     app.post('/plan', (req, res) => {
         const info = req.body;
-        const {support, notification, hours, plan, cost} = info;
-        planCollection.insertOne({support, notification, hours, plan, cost})
+        const { support, notification, hours, plan, cost } = info;
+        planCollection.insertOne({ support, notification, hours, plan, cost })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
 
     // get package plan data
-    app.get('/getPackages', (req, res)=> {
+    app.get('/getPackages', (req, res) => {
         planCollection.find({})
-        .toArray((err, doc) => {
-            res.send(doc)
-        })
+            .toArray((err, doc) => {
+                res.send(doc)
+            })
     })
 
     // get specific package data
-    app.get('/getPackInfo', (req, res)=> {
+    app.get('/getPackInfo', (req, res) => {
         const hr = req.query.hours;
-        planCollection.find({hours: parseInt(hr)})
-        .toArray((err, doc) => {
-            res.send(doc)
-        })
+        planCollection.find({ hours: parseInt(hr) })
+            .toArray((err, doc) => {
+                res.send(doc)
+            })
+    })
+
+    // post employer payment status
+    app.post('/paymentStatus', (req, res) => {
+        const payStatus = req.body;
+        paymentCollection.insertOne(payStatus)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    // get employer payment status
+    app.get('/getPayInfo', (req, res) => {
+        const id = req.query.id;
+        paymentCollection.find({ id: id })
+            .toArray((err, doc) => {
+                res.send(doc)
+            })
+    })
+
+    // update employer payment status and profile
+    app.patch('/updateSubmit', (req, res) => {
+        const id = req.query.id;
+        paymentCollection.updateOne({ id: id },
+            { $set: { email: req.body.email, password: req.body.password } })
+            .then(result => {
+                res.send(result.modifiedCount > 0);
+            })
     })
 
     //  client.close();
